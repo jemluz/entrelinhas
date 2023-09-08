9ddda5eaeea51c6a213fd963b42706bf559d3367
jemluz <contatojemima@gmail.com>
2023-09-08 11:30:50 -0300

    add: navigation layout

--- entrelinhas-vite/src/pages/Invite/index.tsx
+++ entrelinhas-vite/src/pages/Invite/index.tsx
@@ -1,6 +1,16 @@
 import { YoutubeLogo } from "@phosphor-icons/react";
-import { CallToAction, Avatar, Wrapper, LeftTopContainer, LeftTopContent, LeftBottomConainter, LeftBottomContent, GitHubBtn, LinkedinBtn, YoutubeBtn } from "./styles";
+import { CallToAction, Avatar, Wrapper, LeftTopContainer, LeftTopContent, LeftBottomConainter, LeftBottomContent, GitHubBtn, LinkedinBtn, YoutubeBtn, CenterContainer, CenterContent, Dot, LittleDot } from "./styles";
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+
+// const navigation: string[] = [
+//   '.dev',
+//   '0000',
+//   '01001',
+//   '01010',
+//   '10001',
+//   '10110',
+//   '00101'
+// ]
 
 export function Invite() {
   return <>
@@ -25,6 +35,18 @@ export function Invite() {
           </YoutubeBtn>
         </LeftBottomContent>
       </LeftBottomConainter>
+
+      <CenterContainer>
+        <CenterContent>
+          <a href="" className="active"><Dot><LittleDot></LittleDot></Dot>.dev</a>
+          <a href="">0<strong>0</strong>00</a>
+          <a href="">0<strong>1</strong>00<strong>1</strong></a>
+          <a href="">010<strong>1</strong>0</a>
+          <a href="">1<strong>0</strong>001</a>
+          <a href=""><strong>1</strong>011<strong>0</strong></a>
+          <a href="">00<strong>1</strong>01</a>
+        </CenterContent>
+      </CenterContainer>
 
       <CallToAction>
         call to action
--- entrelinhas-vite/src/pages/Invite/styles.ts
+++ entrelinhas-vite/src/pages/Invite/styles.ts
@@ -5,11 +5,16 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 35fr 7fr 23fr 35fr;
-  grid-template-rows: 30fr 20fr 10fr 20fr 10fr;
+  grid-template-columns: 35fr 144px 30fr 27fr;
+  grid-template-rows: 30fr auto 10fr 20fr 10fr;
 
   height: 100vh;
   color: #292929;
+  font-family: 'Changa', sans-serif;
+
+  a {
+    text-decoration: none;
+  }
 `
 
 export const LeftTopContainer = styled.div`
@@ -39,8 +44,8 @@ export const LeftTopContent = styled.div`
 `
 
 export const Avatar = styled.img`
-  height: 8rem;
-  width: 8rem;
+  height: 6rem;
+  width: 6rem;
 
   border-radius: 99px;
   border: 3px solid #E3E3E3;
@@ -59,6 +64,7 @@ export const LeftBottomConainter = styled.div`
 
 export const LeftBottomContent = styled.nav`
   width: 200px;
+  height: 80px;
 
   display: flex;
   justify-content: space-around;
@@ -67,6 +73,8 @@ export const LeftBottomContent = styled.nav`
   a {
     padding: .5rem .6rem .3rem .6rem;
     border-radius: 12px;
+    border: 1px solid transparent;
+    background-color: transparent;
 
     transition: all .2s ease;
 
@@ -83,32 +91,85 @@ export const LeftBottomContent = styled.nav`
 
 export const GitHubBtn = styled.a`
   color: #0A0A0A;
-  background-color: #E6E6E6;
 
   &:hover {
-    color: #fff;
-    background-color: #0A0A0A;
+    color: #0A0A0A;
+    background-color: #E6E6E6;
   }
 `
 
 export const LinkedinBtn = styled.a`
   color: #3880D6;
-  background-color: #EBF2FB;
 
   &:hover {
-    color: #fff;
-    background-color: #3880D6;
+    color: #3880D6;
+    background-color: #EBF2FB;
   }
 `
 
 export const YoutubeBtn = styled.a`
   color: #EF5330;
-  background-color: #FDEEEA;
 
   &:hover {
-    color: #fff;
-    background-color: #EF5330;
+    color: #EF5330;
+    background-color: #FDEEEA;
   }
+`
+
+export const CenterContainer = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 2 / 4;
+
+  display: flex;
+  justify-content: center;
+`
+
+export const CenterContent = styled.div`
+  height: fit-content;
+
+  border-left: 3px solid #E3E3E3;
+
+  display: flex;
+  flex-direction: column;
+
+  a {
+    font-size: 20px;
+    padding: .1rem 1rem;
+    color: #8B8B8B;
+
+    strong {
+      color: #0A0A0A;
+    }
+
+    &.active {
+      background-color: #F3F3F3;
+      border-right: 3px solid #0A0A0A;
+      display: flex;
+      justify-content: center;
+      color: #0A0A0A;
+    }
+  }
+`
+
+export const Dot = styled.span`
+  height: 16px;
+  width: 16px;
+  border: 4px solid #0A0A0A;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: -90px;
+  margin-top: 6px;
+  position: absolute;
+`
+
+export const LittleDot = styled.span`
+  height: 10px;
+  width: 10px;
+  background-color: #A2A2A2;
+  border-radius: 50%;
+  display: inline-block;
 `
 
 export const CallToAction = styled.div`
