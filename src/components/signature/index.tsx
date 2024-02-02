3e2fda8dcaa42434117a796ecfc417d6f5074b80
jemluz <contatojemima@gmail.com>
2024-02-02 15:05:27 -0300

    add: signature component

---
+++ src/components/signature/index.tsx
@@ -0,0 +1,11 @@
+import { Heart } from "phosphor-react";
+import { SignatureContainer } from "./style";
+
+export function Signature() {
+  return (
+    <SignatureContainer>
+      template by jemluz
+      <Heart size={16} weight="fill" />
+    </SignatureContainer>
+  )
+}
---
+++ src/components/signature/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const SignatureContainer = styled.a`
+  color: #000;
+
+  font-family: "Coustard", serif;
+  font-weight: 400;
+  font-size: 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: fit-content;
+
+  svg {
+    margin-left: 0.5rem;
+  }
+`;
