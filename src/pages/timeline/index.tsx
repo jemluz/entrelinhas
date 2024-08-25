7d15449cfd5d1163f686e9ddc5a70ff7b500577d
jemluz <contatojemima@gmail.com>
2024-08-24 23:04:27 -0300

    feat: interpolation added to TimelineCard component
    - create `lessons.ts` file to group all timeline cards data/info
    - replace `TimelineCard` list by mapping lessonsList

--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -1,54 +1,28 @@
+import { lessonsList, lessonTypeEnum } from '../../utils/lessons'
 import { TimelineCard } from './components/timeline-card'
 import { Credits, TimelineContainer } from './styles'
 
-const colors = {
-  accentBlue: '#41516C',
-  accentYellow: '#FBCA3E',
-  accentRed: '#E24A68',
-  accentDarkBlue: '#1B5F8C',
-  accentTeal: '#4CADAD',
-}
-
 export default function Timeline() {
+  console.log()
   return (
     <div>
       <h1>UL timeline cards</h1>
       <TimelineContainer>
-        <TimelineCard
-          date="2002"
-          title="Title 1"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!"
-          accentColor={colors.accentBlue}
-          columnSide="left"
-        />
-        <TimelineCard
-          date="2007"
-          title="Title 2"
-          description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos adipisci nobis nostrum vero nihil veniam."
-          accentColor={colors.accentYellow}
-          columnSide="right"
-        />
-        <TimelineCard
-          date="2012"
-          title="Title 3"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga minima consequuntur soluta placeat iure totam commodi repellendus ea delectus, libero fugit quod reprehenderit, sequi quo, et dolorum saepe nulla hic."
-          accentColor={colors.accentRed}
-          columnSide="left"
-        />
-        <TimelineCard
-          date="2017"
-          title="Title 4"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit, cumque."
-          accentColor={colors.accentDarkBlue}
-          columnSide="right"
-        />
-        <TimelineCard
-          date="2022"
-          title="Title 5"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, non."
-          accentColor={colors.accentTeal}
-          columnSide="left"
-        />
+        {lessonsList.map((lesson) => {
+          const { id, title, subtitle, description, color, lessonType } = lesson
+          return (
+            <TimelineCard
+              key={id}
+              date={title}
+              title={subtitle}
+              description={description}
+              accentColor={color}
+              columnSide={
+                lessonType === lessonTypeEnum.practice ? 'left' : 'right'
+              }
+            />
+          )
+        })}
       </TimelineContainer>
       <Credits>
         <a
---
+++ src/utils/lessons.ts
@@ -0,0 +1,143 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export enum lessonTypeEnum {
+  practice = 'practice',
+  theory = 'theory',
+}
+
+export interface ILesson {
+  id: string
+  title: string
+  subtitle: string
+  description: string
+  color: string
+  lessonType: lessonTypeEnum
+}
+
+const colors = {
+  accentBlue: '#41516C',
+  accentYellow: '#FBCA3E',
+  accentRed: '#E24A68',
+  accentDarkBlue: '#1B5F8C',
+  accentTeal: '#4CADAD',
+}
+
+export const lessonsList: ILesson[] = [
+  {
+    id: uuidv4(),
+    title: 'Introdução',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentBlue,
+    lessonType: lessonTypeEnum.practice,
+  },
+  {
+    id: uuidv4(),
+    title: '',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentYellow,
+    lessonType: lessonTypeEnum.theory,
+  },
+  {
+    id: uuidv4(),
+    title: 'Análise de requisitos',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentRed,
+    lessonType: lessonTypeEnum.practice,
+  },
+  {
+    id: uuidv4(),
+    title: '',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentDarkBlue,
+    lessonType: lessonTypeEnum.theory,
+  },
+  {
+    id: uuidv4(),
+    title: 'Criando repositório',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentTeal,
+    lessonType: lessonTypeEnum.practice,
+  },
+  {
+    id: uuidv4(),
+    title: '',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentDarkBlue,
+    lessonType: lessonTypeEnum.theory,
+  },
+  {
+    id: uuidv4(),
+    title: 'Criando repositório',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentTeal,
+    lessonType: lessonTypeEnum.practice,
+  },
+  {
+    id: uuidv4(),
+    title: '',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentDarkBlue,
+    lessonType: lessonTypeEnum.theory,
+  },
+  {
+    id: uuidv4(),
+    title: 'Criando repositório',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentTeal,
+    lessonType: lessonTypeEnum.practice,
+  },
+  {
+    id: uuidv4(),
+    title: '',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentDarkBlue,
+    lessonType: lessonTypeEnum.theory,
+  },
+  {
+    id: uuidv4(),
+    title: 'Criando repositório',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentTeal,
+    lessonType: lessonTypeEnum.practice,
+  },
+  {
+    id: uuidv4(),
+    title: '',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentDarkBlue,
+    lessonType: lessonTypeEnum.theory,
+  },
+  {
+    id: uuidv4(),
+    title: 'Criando repositório',
+    subtitle: 'Some subtitle',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!',
+    color: colors.accentTeal,
+    lessonType: lessonTypeEnum.practice,
+  },
+]
